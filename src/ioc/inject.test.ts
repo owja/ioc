22ceae456f99dde7210e035bc104fdceb58d1127
Hauke Broer <info@owja.de>
2019-05-20 09:41:17 +0200

    add tests for circular dependency

--- src/ioc/inject.test.ts
+++ src/ioc/inject.test.ts
@@ -9,12 +9,22 @@ interface ITestClass {
     childTwo?: ITestClass;
 }
 
+interface ICircular {
+    name: string;
+    circular?: ICircular;
+    circularName: string;
+}
+
 const TYPE = {
     "parent": Symbol.for("parent"),
     "child1": Symbol.for("child1"),
     "child2": Symbol.for("child2"),
     "child3": Symbol.for("child3"),
     "child4": Symbol.for("child4"),
+    "circular1": Symbol.for("circular1"),
+    "circular2": Symbol.for("circular2"),
+    "circularFail1": Symbol.for("circularFail1"),
+    "circularFail2": Symbol.for("circularFail2"),
 };
 
 class Parent implements ITestClass {
@@ -51,18 +61,57 @@ class ChildFour implements ITestClass {
     name = "child four";
 }
 
+class Circular1 implements ICircular {
+    @inject(TYPE.circular2, container)
+    circular!: ICircular;
+    get circularName(): string {
+        return this.circular.name;
+    }
+    constructor(public name: string) {}
+}
+
+class Circular2 implements ICircular {
+    @inject(TYPE.circular1, container)
+    circular!: ICircular;
+    get circularName(): string {
+        return this.circular.name;
+    }
+    constructor(public name: string) {}
+}
+
+class CircularFail1 implements ICircular {
+    @inject(TYPE.circularFail2, container)
+    circular!: ICircular;
+    circularName: string = "";
+    constructor(public name: string) {
+        this.circularName = this.circular.name;
+    }
+}
+
+class CircularFail2 implements ICircular {
+    @inject(TYPE.circularFail1, container)
+    circular!: ICircular;
+    circularName: string = "";
+    constructor(public name: string) {
+        this.circularName = this.circular.name;
+    }
+}
+
 container.bind<ITestClass>(TYPE.parent).to(Parent);
 container.bind<ITestClass>(TYPE.child1).to(ChildOne);
 container.bind<ITestClass>(TYPE.child2).to(ChildTwo);
 container.bind<ITestClass>(TYPE.child3).to(ChildThree);
 container.bind<ITestClass>(TYPE.child4).to(ChildFour);
+container.bind<ICircular>(TYPE.circularFail1).toFactory(() => new CircularFail1("one"));
+container.bind<ICircular>(TYPE.circularFail2).toFactory(() => new CircularFail2("two"));
+container.bind<ICircular>(TYPE.circular1).toFactory(() => new Circular1("one"));
+container.bind<ICircular>(TYPE.circular2).toFactory(() => new Circular2("two"));
 
 describe("Injector", () => {
 
     let instance: Parent;
 
     beforeEach(() => {
-
         instance = new Parent();
     });
 
@@ -80,6 +129,18 @@ describe("Injector", () => {
     test("can inject parent", () => {
         expect((instance.childOne.childOne as any).childOne.name).toBe("child one");
         expect((instance.childOne.childTwo as any).childTwo.name).toBe("parent");
+    });
+
+    test("can inject a circular dependency when accessing the dependency outside of constructor", () => {
+        const instance1 = container.get<ICircular>(TYPE.circular1);
+        const instance2 = container.get<ICircular>(TYPE.circular2);
+
+        expect(instance1.circularName).toBe("two");
+        expect(instance2.circularName).toBe("one");
+    });
+
+    test("can not inject a circular dependency when accessing the dependency inside of constructor", () => {
+        expect(() => container.get<ICircular>(TYPE.circularFail1)).toThrow("Maximum call stack size exceeded");
     });
 
 });
