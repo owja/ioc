0aceec0ea7ee9835093e5d8a65fa93e9922ca7c7
Hauke Broer <info@owja.de>
2019-05-24 21:35:25 +0200

    configurable cache

--- src/ioc/inject.ts
+++ src/ioc/inject.ts
@@ -1,16 +1,28 @@
 import {Container} from "./container";
 
-function inject(type: symbol, container: Container) {
+export const NOCACHE = Symbol.for("NOCACHE");
+
+function inject(type: symbol, container: Container, args?: symbol[]) {
     return function(target: object, property: string): void {
         Object.defineProperty(target, property, {
-            get: () => container.get<any>(type),
+            get: function() {
+                const value = container.get<any>(type);
+                if (args && args.indexOf(NOCACHE)) {
+                    Object.defineProperty(this, property, {
+                        value,
+                        enumerable: true,
+                    });
+                }
+                return value;
+            },
+            configurable: true,
             enumerable: true,
         });
     };
 }
 
 export function createDecorator(container: Container) {
-    return function(type: symbol) {
-        return inject(type, container);
+    return function(type: symbol, ...args: symbol[]) {
+        return inject(type, container, args);
     };
 }
