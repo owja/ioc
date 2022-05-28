4b4e996e5215c39bdc32e7a570e2b6a5b0ab825b
Hauke Broer <hauke.broer@ise.de>
2022-05-29 00:57:58 +0200

    fix args are never tokens; make some more types more readable

--- src/ioc/decorator.ts
+++ src/ioc/decorator.ts
@@ -3,7 +3,7 @@ import {define} from "./define";
 import {MaybeToken} from "./token";
 
 export function createDecorator(container: Container) {
-    return <T>(token: MaybeToken<T>, ...args: MaybeToken[]) => {
+    return <T>(token: MaybeToken<T>, ...args: symbol[]) => {
         return <Target extends {[key in Prop]: T}, Prop extends string>(target: Target, property: Prop): void => {
             define(target, property, container, token, args);
         };
