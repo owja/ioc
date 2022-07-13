16f22ee2a60ce32aa796a7d31bdec9db9a84c59f
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-07-14 01:20:34 +0200

    add every type to index exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {Container} from "./ioc/container";
-export {Plugin} from './ioc/types';
+export type {Plugin, Factory, Item, NewAble, Registry, Token, Value} from './ioc/types';
 export {createDecorator} from "./ioc/decorator";
 export {createWire} from "./ioc/wire";
 export {createResolve} from "./ioc/resolve";
