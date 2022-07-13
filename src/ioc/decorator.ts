25eb2858b433adc23a907ab8942c19b8339d0431
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-07-13 15:03:28 +0200

    changing this arrowFunction reduces build size

--- src/ioc/decorator.ts
+++ src/ioc/decorator.ts
@@ -4,7 +4,7 @@ import type {MaybeToken} from "./types";
 
 export function createDecorator(container: Container) {
     return <T>(token: MaybeToken<T>, ...args: symbol[]) => {
-        return <Target extends {[key in Prop]: T}, Prop extends string>(target: Target, property: Prop): void => {
+        return function <Target extends {[key in Prop]: T}, Prop extends string>(target: Target, property: Prop): void {
             define(target, property, container, token, args);
         };
     };
