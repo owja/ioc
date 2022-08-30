2e6dca315295a7b0222f605c37f63dba9b898cce
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-08-30 11:03:33 +0200

    `npm run prettier:fix`

--- src/ioc/token.ts
+++ src/ioc/token.ts
@@ -2,7 +2,8 @@ import type {MaybeToken, Token} from "./types";
 
 export const token = <T, U extends Array<unknown> = unknown[]>(name: string) => ({type: Symbol(name)} as Token<T, U>);
 
-const isToken = <T, U extends Array<unknown>>(token: MaybeToken<T, U>): token is Token<T, U> => typeof token != "symbol";
+const isToken = <T, U extends Array<unknown>>(token: MaybeToken<T, U>): token is Token<T, U> =>
+    typeof token != "symbol";
 
 export const stringifyToken = (token: MaybeToken): string =>
     isToken(token) ? `Token(${token.type.toString()})` : token.toString();
